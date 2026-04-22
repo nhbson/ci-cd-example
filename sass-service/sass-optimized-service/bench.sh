3d79650c43ff3cb2d053cb01b8c56ba0bfa0d03b
SonDYM <nhbsondym@gmail.com>
2026-04-22 14:38:47 +0700

    A.I testing example scale up 5 app

--- sass-service/sass-optimized-service/bench.sh
+++ sass-service/sass-optimized-service/bench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Runs benchmark from WITHIN the docker network to bypass Windows latency
-docker run --rm --network sass-optimized-service_sass-network williamyeh/wrk -t16 -c400 -d30s http://nginx/ping
+docker run --rm --network sass-optimized-service_sass-network williamyeh/wrk -t16 -c400 -d30s http://nginx/api/ping
--- sass-service/sass-optimized-service/src/routes/api.php
+++ sass-service/sass-optimized-service/src/routes/api.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', fn() => 'ok');
+Route::get('/ping', fn() => 'ok!');
